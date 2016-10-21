7c91a0f7dc36b6617d547587a68a5cf3d6a3c4bc
Patrick Sarson <sarsonmedia@gmail.com>
2016-10-21 13:52:23 -0400

    complete myqueue

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -4,15 +4,23 @@ class MyQueue
 
   def initialize
     @queue = Array.new
-    @head = @queue[0]
+    @head = ""
+    @tail = ""
   end
 
   def enqueue(element)
+    @queue.unshift(element)
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    if (!empty?)
+      @queue.pop
+    end
   end
 
   def empty?
+    @queue.length <= 0
   end
 end
