7370e4fc2b7bba5bf6a89f324fc10f6fc49a7399
Patrick Sarson <sarsonmedia@gmail.com>
2016-10-21 14:07:18 -0400

    complete screen

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -8,21 +8,27 @@ class Screen
   def initialize(width, height)
     self.width= width
     self.height = height
+    @matrix = Array.new
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
-    return pixel, x, y
+    @matrix[x] = []
+    @matrix[x][y] = pixel
   end
 
   def at(x, y)
-
+    if (inbounds(x, y))
+	  		return nil
+	  	else
+	     	@matrix[x][y]
+	    end
   end
 
   private
 
   def inbounds(x, y)
-
+    (x < 0 || y < 0)
   end
 
 end
