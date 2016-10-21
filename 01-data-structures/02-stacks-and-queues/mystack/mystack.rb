1adad2560a425dbc3dac9401f73623d49dc5a08e
Patrick Sarson <sarsonmedia@gmail.com>
2016-10-21 13:42:46 -0400

    complete mystack

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -1,17 +1,25 @@
 class MyStack
   attr_accessor :top
+  attr_accessor :stack
 
   def initialize
-    @stack = Array.new
+    self.stack = Array.new
     self.top = nil
   end
 
   def push(item)
+    self.stack.unshift(item)
+    self.top = @stack[0]
   end
 
   def pop
+    popped = self.stack.slice!(0)
+    self.top = @stack[0]
+    return popped
   end
 
   def empty?
+    self.stack.length <=  0
   end
+
 end
