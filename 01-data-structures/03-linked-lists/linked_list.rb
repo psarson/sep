8f3adc27c84973ff2614032bbb015cb9d2c0417e
Patrick Sarson <sarsonmedia@gmail.com>
2016-10-21 22:49:48 -0400

    complete linked lists

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -1,30 +1,55 @@
-require_relative 'node'
+class LinkedList < Node
 
-class LinkedList
-  attr_accessor :head
-  attr_accessor :tail
+  def initialize
+    @next = @prev = self
+  end
 
-  # This method creates a new `Node` using `data`, and inserts it at the end of the list.
+  def head
+    @next unless @next == self
+  end
+
+  def tail
+    @prev unless @prev == self
+  end
+
   def add_to_tail(node)
+    node.insert_after(self.prev)
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+  	tail.remove
   end
 
   # This method prints out a representation of the list.
+  def each(&block)
+    return enum_for(:each) unless block_given?
+    node = @next
+    while node != self
+      yield node
+      node = node.next
+    end
+  end
+
   def print
+    array = each.collect {|node| node.data }
+    $stdout.puts array.join("\n")
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    node.remove
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+     node.insert_after(self)
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    return @next if @next == self
+    head.remove
   end
+
 end
--- 01-data-structures/03-linked-lists/node.rb
+++ 01-data-structures/03-linked-lists/node.rb
@@ -1,7 +1,22 @@
 class Node
-  attr_accessor :next
-  attr_accessor :data
+  attr_accessor :next, :prev, :data
 
   def initialize(data)
+    @data = data
+  end
+
+  def remove
+    @prev.next = @next if @prev
+    @next.prev = @prev if @next
+    @next = @prev = nil
+  end
+
+  def insert_after(node)
+    remove
+
+    @next = node.next
+    @next.prev = self if @next
+    @prev = node
+    node.next = self
   end
 end
